0dd973f768e10f43753422bd48655b45f2e923cf
Roman228-web <swgimbarr@yandex.ru>
2022-04-16 19:33:57 +0300

    class to function

--- reactpizza/src/Button.jsx
+++ reactpizza/src/Button.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import classNames from "classnames";
-class Button extends React.Component {
-  render() {
-    return (
-      <button
-        className={this.props.outline ? "button button--outline" : "button"}
-      >
-        {this.props.children}
-      </button>
-    );
-  }
+function Button(props) {
+  return (
+    <button
+      className={classNames("button", {
+        "button--outline": props.outline,
+      })}
+    >
+      {props.children}
+    </button>
+  );
 }
-
 // function Button() {
 //   return <button>Hello</button>;
 // }
